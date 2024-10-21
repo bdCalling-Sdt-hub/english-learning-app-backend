1de109cdc3bf3273120efa89c2f7d76fcd5d73f9
Tamim369 <ashiqurrahmantamim369@gmail.com>
2024-10-21 12:44:06 +0600

    added skills

--- src/app/modules/teacher/skills/skills.service.ts
+++ src/app/modules/teacher/skills/skills.service.ts
@@ -1,13 +1,19 @@
 import { StatusCodes } from 'http-status-codes';
 import { Teacher } from '../teacher.model';
 import ApiError from '../../../../errors/ApiError';
-const addSkillToDB = async (id: string, skill: string) => {
+const addSkillToDB = async (id: string, skill: string | string[]) => {
   if (!id) {
     throw new ApiError(StatusCodes.BAD_REQUEST, 'Teacher id is required');
   }
   const isExistTeacher = await Teacher.findById(id);
   if (!isExistTeacher) {
     throw new ApiError(StatusCodes.NOT_FOUND, 'Teacher not found');
+  }
+  if (Array.isArray(skill)) {
+    const result = await Teacher.findByIdAndUpdate(id, {
+      $addToSet: { skills: { $each: skill } },
+    });
+    return result;
   }
   const result = await Teacher.findByIdAndUpdate(id, {
     $push: { skills: skill },
